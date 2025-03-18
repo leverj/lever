be1a6d4d2da581d6cfc37c583e918f99c3a9a7ab
Benny Sadeh <benny.sadeh@gmail.com>
2025-03-18 08:56:52 -0700

    exporting chainsById

--- packages/chain-deployment/scripts/save_networks.js
+++ packages/chain-deployment/scripts/save_networks.js
@@ -1,8 +1,13 @@
+import {Map} from 'immutable'
+import {execSync} from 'node:child_process'
 import {writeFileSync} from 'node:fs'
 import {inspect} from 'util'
 import * as chains from 'viem/chains'
-import {execSync} from 'node:child_process'
+
 const targetDir = `${import.meta.dirname}/../src`
+
+const chainsById = Map(chains).mapEntries(([label, _]) => [_.id, label]).toJS()
+delete chainsById.undefined
 const networks = {}
 for (const [label, network] of Object.entries(chains)) {
   const {id, name, nativeCurrency, rpcUrls, blockExplorers, testnet = false} = network
@@ -11,5 +16,8 @@ for (const [label, network] of Object.entries(chains)) {
   const contracts = {}
   networks[label] = {id: BigInt(id), label, name, nativeCurrency, providerURL, blockExplorer, contracts, testnet: testnet || label === 'hardhat' || label === 'localhost'}
 }
-writeFileSync(`${targetDir}/networks.js`, `export const networks = ${inspect(networks, {showHidden: false, compact: false, depth: null})}`)
+writeFileSync(`${targetDir}/networks.js`,
+`export const networks = ${inspect(networks, {showHidden: false, compact: false, depth: null})}\n
+export const chainsById = ${inspect(chainsById, {showHidden: false, compact: false, depth: null})}\n`)
+
 execSync('curl https://raw.githubusercontent.com/blockscout/chainscout/main/data/chains.json -o ../src/chainscout-chains.json')
