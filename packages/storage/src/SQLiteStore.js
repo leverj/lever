fe4f7b62ad114fdf5cad81f1924a1e1e53ebbc9a
Benny Sadeh <benny.sadeh@gmail.com>
2025-05-10 15:04:08 -0700

    SQLiteStore; using value instead of payload

--- packages/storage/src/SQLiteStore.js
+++ packages/storage/src/SQLiteStore.js
@@ -5,16 +5,16 @@ import {Store} from './Store.js'
 export class SQLiteStore extends Store {
   constructor(db, type) {
     super()
-    db.exec(`CREATE TABLE IF NOT EXISTS ${type} (key TEXT PRIMARY KEY, payload TEXT NOT NULL);`)
+    db.exec(`CREATE TABLE IF NOT EXISTS ${type} (key TEXT PRIMARY KEY, value TEXT NOT NULL);`)
     this.queries = {
-      get: db.prepare(`SELECT payload FROM ${type} WHERE key = ?`),
-      set: db.prepare(`INSERT INTO ${type} (key, payload) VALUES (?, ?)`),
-      update: db.prepare(`UPDATE ${type} SET payload = ? WHERE key = ?`),
+      get: db.prepare(`SELECT value FROM ${type} WHERE key = ?`),
+      set: db.prepare(`INSERT INTO ${type} (key, value) VALUES (?, ?)`),
+      update: db.prepare(`UPDATE ${type} SET value = ? WHERE key = ?`),
       delete: db.prepare(`DELETE from ${type} WHERE key = ?`),
-      find: db.prepare(`SELECT payload FROM ${type} WHERE key LIKE CONCAT(?,'%')`),
+      find: db.prepare(`SELECT value FROM ${type} WHERE key LIKE CONCAT(?,'%')`),
       size: db.prepare(`SELECT COUNT(DISTINCT key) AS size FROM ${type}`),
       keys: db.prepare(`SELECT key FROM ${type}`),
-      values: db.prepare(`SELECT payload FROM ${type}`),
+      values: db.prepare(`SELECT value FROM ${type}`),
       entries: db.prepare(`SELECT * FROM ${type}`),
       clear: db.prepare(`DELETE FROM ${type}`),
     }
@@ -23,7 +23,7 @@ export class SQLiteStore extends Store {
   /*** API ***/
   get(key) {
     const found = this.queries.get.get(normalize(key))
-    return found ? deserialize(found.payload) : undefined
+    return found ? deserialize(found.value) : undefined
   }
   set(key, value) { this.queries.set.run(normalize(key), serialize(value)) }
   update(key, value) {
@@ -33,11 +33,11 @@ export class SQLiteStore extends Store {
   }
   delete(key) { this.queries.delete.run(normalize(key)) }
   has(key) { return !!this.get(key) }
-  find(keyable) { return this.queries.find.all(normalize(keyable)).map(_ => deserialize(_.payload)) }
+  find(keyable) { return this.queries.find.all(normalize(keyable)).map(_ => deserialize(_.value)) }
   size() { return this.queries.size.get().size }
   keys() { return this.queries.keys.all().map(_ => _.key) }
-  values() { return this.queries.values.all().map(_ => deserialize(_.payload)) }
-  entries() { return this.queries.entries.all().map(_ => [_.key, deserialize(_.payload)]) }
+  values() { return this.queries.values.all().map(_ => deserialize(_.value)) }
+  entries() { return this.queries.entries.all().map(_ => [_.key, deserialize(_.value)]) }
   toObject() { return Map(this.entries()).toJS() }
   clear() { this.queries.clear.run() }
 }
