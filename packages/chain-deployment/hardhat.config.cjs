a4f6321d139d131be0285ef65d94ac42c9c2a2a5
Benny Sadeh <benny.sadeh@gmail.com>
2025-08-20 11:48:56 -0700

    upgrade hardhat

--- packages/chain-deployment/hardhat.config.cjs
+++ packages/chain-deployment/hardhat.config.cjs
@@ -4,11 +4,24 @@ require('@nomicfoundation/hardhat-verify')
 require('hardhat-deploy-ethers')
 
 module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.28',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 15000
+          },
+          evmVersion: 'cancun'
+        }
+      },
+    ],
+  },
   networks: {
     hardhat: {
       gasPrice: 0,
       initialBaseFeePerGas: 0,
     },
   },
-  solidity: '0.8.24',
 }
