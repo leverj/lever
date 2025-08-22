9cd857346dce73a26c550096c088ef6429014715
Benny Sadeh <benny.sadeh@gmail.com>
2025-08-22 08:29:15 -0700

    restoring previous solidity compilation

--- packages/chain-deployment/hardhat.config.cjs
+++ packages/chain-deployment/hardhat.config.cjs
@@ -4,20 +4,7 @@ require('@nomicfoundation/hardhat-verify')
 require('hardhat-deploy-ethers')
 
 module.exports = {
-  solidity: {
-    compilers: [
-      {
-        version: '0.8.28',
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 15000
-          },
-          evmVersion: 'cancun'
-        }
-      },
-    ],
-  },
+  solidity: '0.8.28',
   networks: {
     hardhat: {
       gasPrice: 0,
