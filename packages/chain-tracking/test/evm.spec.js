f48d70baa92f91c7aaf210569d4eabd778c38704
Benny Sadeh <benny.sadeh@gmail.com>
2025-08-31 22:49:19 -0700

    chainscout-chains update

--- packages/chain-tracking/test/evm.spec.js
+++ packages/chain-tracking/test/evm.spec.js
@@ -5,7 +5,7 @@ import {ERC20, provider} from './help.js'
 describe('evm', () => {
   it('getCreationBlock', async () => {
     const before = await provider.getBlockNumber()
-    for (let i = 0; i <= 3; i++) {
+    for (let i = 1; i <= 3; i++) {
       const contract = await ERC20()
       expect(await getCreationBlock(provider, contract.target, 10)).toEqual(before + i)
     }
