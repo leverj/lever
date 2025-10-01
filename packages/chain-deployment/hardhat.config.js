76aff0660d62a58dcd2d400b9a05647a439df447
Benny Sadeh <benny.sadeh@gmail.com>
2025-10-01 16:48:44 -0700

    adding default profile in hardhat config

--- packages/chain-deployment/hardhat.config.js
+++ packages/chain-deployment/hardhat.config.js
@@ -15,21 +15,21 @@ export default {
     hardhat_verify,
   ],
   solidity: { // changing these values affects deployment address
-    compilers: [
-      {
+    profiles: {
+      default: {
         version: '0.8.30',
         settings: {
           optimizer: {enabled: true, runs: 200, details: {yul: true}},
           viaIR: true, //see: https://docs.soliditylang.org/en/latest/ir-breaking-changes.html
         },
       },
-    ],
-    profiles: {
       production: {
+        version: '0.8.30',
         settings: {
-          optimizer: {runs: 1_000_000},
+          optimizer: {enabled: true, runs: 1_000_000, details: {yul: true}},
+          viaIR: true, //see: https://docs.soliditylang.org/en/latest/ir-breaking-changes.html
         },
-       },
+      },
     },
   },
 }
