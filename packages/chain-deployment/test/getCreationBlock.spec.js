05a9f9dddf98799c9b44455d989fdb7fdeb603e5
Benny Sadeh <benny.sadeh@gmail.com>
2025-10-12 13:33:50 -0700

    getCreationBlock test moved to chain-deployment

--- packages/chain-deployment/test/getCreationBlock.spec.js
+++ packages/chain-deployment/test/getCreationBlock.spec.js
@@ -2,8 +2,8 @@ import {getCreationBlock} from '@leverj/lever.common'
 import {expect} from 'expect'
 import {ERC20, provider} from './help.js'
 
-describe('evm', () => {
-  it('getCreationBlock', async () => {
+describe('getCreationBlock', () => {
+  it('can get the creation block of a contract', async () => {
     const before = await provider.getBlockNumber()
     for (let i = 1; i <= 3; i++) {
       const contract = await ERC20()
